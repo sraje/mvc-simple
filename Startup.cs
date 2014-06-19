da6bdbed76a0b9e88f7d32d919f6b49150d8e1fa
sraje <sraje@umail.ucsb.edu>
2014-06-19 14:06:44 -0700

    start
    start

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_simple.Startup))]
+namespace mvc_simple
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
